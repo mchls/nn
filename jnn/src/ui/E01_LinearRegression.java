fe41a9630ccedd5f871b6deba7234caef9cb8b65
Michal <michal.steuer@gmail.com>
2015-07-27 23:01:00 +0200

    Zakladni popis

--- jnn/src/ui/E01_LinearRegression.java
+++ jnn/src/ui/E01_LinearRegression.java
@@ -1,5 +1,10 @@
 package ui;
 
+/**
+ * http://ufldl.stanford.edu/tutorial/supervised/LinearRegression/
+ * 
+ * @author michal
+ */
 public class E01_LinearRegression {
     
 }
--- jnn/src/ui/Main.java
+++ jnn/src/ui/Main.java
@@ -1,9 +1,16 @@
 package ui;
 
+/**
+ * http://ufldl.stanford.edu/tutorial/
+ * Starter code: git clone https://github.com/amaas/stanford_dl_ex
+ * Data should go to the common folder: http://ai.stanford.edu/~amaas/data/data.zip
+ * 
+ * @author michal
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world edited from web!");
+        System.out.println("Hello world edited from the web!");
     }
     
 }
